20e0241f38c5aaa3f85fef6376279bac93509eae
scihacker <sjtuzlp@gmail.com>
2024-08-26 22:54:16 +1000

    [1.0.1]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pce',
-    version='1.0.0',
+    version='1.0.1',
     description='PCETools, a basic library for PCE softwares',
     author='Scihacker',
     author_email='sjtuzlp@gmail.com',
